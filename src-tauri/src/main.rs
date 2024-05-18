d77a7f191a1e9248373f01d079263ab24d7b8a92
CarOL <herixth@163.com>
2024-05-18 15:41:45 +0800

    [add] obj loader and render

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,26 +3,29 @@
     windows_subsystem = "windows"
 )]
 
+use std::fs::File;
+use std::io::BufReader;
+use obj::{load_obj, Obj};
 use tauri::api::shell;
 use tauri::{CustomMenuItem, Manager, Menu, Submenu};
 
 #[tauri::command]
-fn backend_add(number: i32) -> i32 {
-    // Note: these commands block the main thread and hang the UI until they return.
-    // If you need to run a long-running task, use async command instead.
-    println!("Backend was called with an argument: {}", number);
-    number + 2
-}
-
-#[tauri::command]
-fn backend_get_obj() -> String {
-    String::from("static/tooth.obj")
+fn backend_load_obj(file_path: String) -> Obj {
+    let input = BufReader::new(match File::open(&file_path) {
+        Err(why) => panic!("couldn't open {:?}", why),
+        Ok(file) => file,
+    });
+    let model: Obj = match load_obj(input) {
+        Err(why) => panic!("couldn't load {:?}", why),
+        Ok(model) => model,
+    };
+    model
 }
 
 fn main() {
     let ctx = tauri::generate_context!();
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![backend_add, backend_get_obj])
+        .invoke_handler(tauri::generate_handler![backend_load_obj])
         .menu(
             tauri::Menu::os_default("Tauri Vue Template").add_submenu(Submenu::new(
                 "Help",
--- src/components/ThreeScene.vue
+++ src/components/ThreeScene.vue
@@ -5,15 +5,16 @@
 
 <script>
 import * as THREE from "three";
-import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { VertexNormalsHelper } from 'three/addons/helpers/VertexNormalsHelper.js';
 import Stats from "stats-js";
-import { invoke } from '@tauri-apps/api/tauri'
+import { invoke } from "@tauri-apps/api/tauri"
+import { listen } from "@tauri-apps/api/event"
 
-async function backendGetOBJ() {
-  return await invoke('backend_get_obj');
-}
-
+// async function backendLoadOBJ() {
+//   return await 
+// }
 
 export default {
   //import引入的组件需要注入到对象中才能使用
@@ -46,6 +47,8 @@ export default {
       this.renderer.setSize(window.innerWidth, window.innerHeight);
       this.renderer.setClearColor(0xffffff, 1);
       this.renderer.setPixelRatio(window.devicePixelRatio);
+      this.renderer.shadowMap.enabled = true;
+      // this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
       document.getElementById("container").appendChild(this.renderer.domElement);
 
       // 创建坐标轴
@@ -58,40 +61,68 @@ export default {
       this.camera.lookAt(this.scene.position);
       this.scene.add(this.camera);
 
-      const ambientLight = new THREE.AmbientLight(0xffffff, 0.4);
+      const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
       this.scene.add(ambientLight);
       const pointLight = new THREE.PointLight(0xffffff, 1.0)
+      pointLight.position.set(30, 30, 30);
+      pointLight.castShadow = true;
       this.scene.add(pointLight);
 
       // 控制器，监听鼠标事件
       this.controls = new OrbitControls(this.camera, this.renderer.domElement);
-      this.controls.addEventListener("change", ()=>{
+      this.controls.addEventListener("change", () => {
         this.renderer.render(this.scene, this.camera);
       });
-      window.addEventListener("resize", ()=>{
+      window.addEventListener("resize", () => {
         this.camera.aspect = window.innerWidth / window.innerHeight;
         this.camera.updateProjectionMatrix();
         this.renderer.setSize(window.innerWidth, window.innerHeight);
       }, false);
+
+      listen('tauri://file-drop', event => {
+        const filePath = event['payload'][0];
+        if (filePath.endsWith('.obj')) {
+          console.log(filePath);
+          this.loadOBJ(filePath);
+        }
+      });
     },
     renderScene() {
       this.renderer.render(this.scene, this.camera);
     },
-    loadOBJ(filepath) {
-      new OBJLoader().load(
-        filepath, (object) => {
-          this.scene.add(object);
-          this.renderScene();
-        },
-        (xhr) => {
-          console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-        },
-        (error) => {
-          console.log(error);
-        }
-      );
-    },
     animate() { },
+    loadOBJ(filePath) {
+      invoke('backend_load_obj', { filePath: filePath }).then((Obj) => {
+        // flatten position and normal
+        const positions = [];
+        const normals = [];
+
+        for (const vertex of Obj.vertices) {
+          positions.push(...vertex.position);
+          normals.push(...vertex.normal);
+        }
+
+        var geometry = new THREE.BufferGeometry();
+
+        geometry.setAttribute('position', new THREE.BufferAttribute(new Float32Array(positions), 3));
+        geometry.setAttribute('normal', new THREE.BufferAttribute(new Float32Array(normals), 3));
+
+        geometry.setIndex(Obj.indices);
+
+        const material = new THREE.MeshLambertMaterial({
+          color: new THREE.Color("rgb(250, 250, 250)"),
+          side: THREE.DoubleSide
+        });
+
+        const mesh = new THREE.Mesh(geometry, material);
+        mesh.receiveShadow = true;
+
+        const helper = new VertexNormalsHelper(mesh);
+        this.scene.add(mesh);
+
+        this.renderScene();
+      });
+    }
   },
   //生命周期 - 创建完成（可以访问当前this实例）
   created() {
@@ -101,9 +132,6 @@ export default {
     this.init();
     this.renderScene();
     // this.loadOBJ('static/tooth.obj');
-    backendGetOBJ().then(objPath => {
-      this.loadOBJ(objPath);
-    });
   },
   beforeCreate() { }, //生命周期 - 创建之前
   beforeMount() { }, //生命周期 - 挂载之前
